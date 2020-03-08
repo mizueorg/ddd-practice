d78aa1c3ad18e646c392275f94aeac3e91521133
ca-mizushima <mizushima@cloud-ace.jp>
2020-03-08 16:07:49 +0900

    UserIDにルールを追加

--- domain/value_object/user_id.go
+++ domain/value_object/user_id.go
@@ -1,3 +1,12 @@
 package value_object
 
+import "errors"
+
 type UserID string
+
+func (u *UserID) Verify() error {
+	if string(*u) == "" {
+		return errors.New("user id is not empty")
+	}
+	return nil
+}
---
+++ domain/value_object/user_id_test.go
@@ -0,0 +1,29 @@
+package value_object
+
+import "testing"
+
+func TestUserID_Verify(t *testing.T) {
+	tests := []struct {
+		name    string
+		u       UserID
+		wantErr bool
+	}{
+		{
+			name:    "OK",
+			u:       "id1",
+			wantErr: false,
+		},
+		{
+			name:    "NG",
+			u:       "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.u.Verify(); (err != nil) != tt.wantErr {
+				t.Errorf("Verify() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
