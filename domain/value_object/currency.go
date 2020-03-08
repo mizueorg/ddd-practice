7deb25f11cce1af08ec88ba848c9a69b672ad493
qushot <moto@yoshidumi.co.jp>
2020-03-08 15:15:10 +0900

    add currency and money

---
+++ domain/value_object/currency.go
@@ -0,0 +1,15 @@
+package value_object
+
+//go:generate stringer -type Currency ./currency.go
+type Currency int
+
+const (
+	_ Currency = iota
+	JPY
+	USD
+	EUR
+)
+
+func (i Currency) IsUndefined() bool {
+	return i < JPY || i > EUR
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/trewanek-org/ddd-practice
+
+go 1.13
